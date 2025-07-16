098fdc5b96b0f0e79302bf4f1f771ab4ff155a4c
HXHdevs <jordisonnicolas@gmail.com>
2025-07-15 20:23:36 -0600

    Operadores aritmeticos en Python

---
+++ python/Operadores.py
@@ -0,0 +1,48 @@
+print("Suma")
+var1 = 5
+var2 = 5
+res=var1+var2
+print("El resultado de la suma es "+str(res))
+
+print("---------------------------------")
+print("Resta")
+varest1 = 5
+varest2 = 1
+rest=varest1-varest2
+print("El resultado de la resta es "+str(rest))
+
+print("---------------------------------")
+print("Multiplicacion")
+varmult1 = 5
+varmult2 = 5
+resmul=varmult1*varmult2
+print("El resultado de la multiplicacion es "+str(resmul))
+
+print("---------------------------------")
+print("Exponencia")
+varpot1 = 5
+varpot2 = 3
+respot=varpot1**varpot2
+print("El resultado de la exponencia es "+str(respot))
+
+print("---------------------------------")
+print("Division")
+vardiv1 = 45
+vardiv2 = 4 
+resdiv=vardiv1/vardiv2
+print("El resultado de la division es "+str(resdiv))
+
+print("---------------------------------")
+print("Division entera")
+vardiv1 = 45
+vardiv2 = 4 
+resdiv=vardiv1//vardiv2
+print("El resultado de la division entera es "+str(resdiv))
+
+print("---------------------------------") #es la cantidad sobrante despues de una division es decir cuando un numero no puede ser dividido eexacatamente por otro
+print("Modulo")
+varmod1 = 30
+varmod2 = 8
+resmod=varmod1%varmod2
+print("El resultado del modulo es "+str(resmod))
+
