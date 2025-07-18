8c4c09df5243040b3f16dc5df00785d0fdf6ce5f
HXHdevs <jordisonnicolas@gmail.com>
2025-07-17 20:33:53 -0600

    Tipos de datos en Python

---
+++ python/Datatypes.py
@@ -0,0 +1,37 @@
+"""
+En Python existen los siguientes tipos de datos
+1.Enteros y largos
+Son los que no tienen decimales ya sean positivos o negativos
+se representan con int y long para los largos
+2.Flotantes
+Numeros con decimales ya sean positivos o negativos y se representan con float
+3.Numeros complejos
+Son los que tienen una parte real y una imaginaria usados mayormente en ingenieria y ciencias
+se representan con complex
+4.Cadenas 
+Son ls que estan compuestos por cadenas
+se representa entre comillas despues de ser declarada
+5.Booleanos
+Mayormente usadas para bucles y condicionales
+se expresa mediante el tipo bool
+"""
+
+#Tipo entero o largo
+numero = 15
+print(numero, type(numero))
+
+#Tipo flotante
+numero_flotante = 15.7
+print(numero_flotante, type(numero_flotante))
+
+#Tipo de dato numero complejo
+numero_compplejo = 15+7j
+print(numero_compplejo, type(numero_compplejo))
+
+#Tipo de dato String
+nombre = "Gon Freecs"
+print(nombre, type(nombre))
+
+#Tipo booleano
+trueorfalse = 3==3
+print(trueorfalse, type(trueorfalse))
